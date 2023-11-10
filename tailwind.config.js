c8599cfe2bd0cb4927eb156a89d61365dc285831
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-11-10 20:42:26 +0530

    chore: configured tailwind classes with brand guidelines

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ export default {
       backgroundImage: {
         "brand-notification":
           "linear-gradient(180deg, rgba(255, 255, 255, 0.20) 27.5%, rgba(22, 93, 255, 0.16) 157.71%)",
+        "brand-auth":
+          "radial-gradient(92.19% 89.74% at 32.27% 91.27%, rgba(31, 169, 17, 0.81) 17.21%, #2F15D0 64.58%, rgba(0, 0, 0, 0.00) 100%)",
+        "brand-auth-layout-notified":
+          "linear-gradient(90deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.92) 27.6%, #000 53.13%, rgba(0, 0, 0, 0.67) 81.77%, rgba(0, 0, 0, 0.00) 100%)",
       },
       colors: {
         "brand-dark-grey": "#19191A",
@@ -23,6 +27,9 @@ export default {
           "0px 1px 1px 0px rgba(0, 0, 0, 0.04), 0px 4px 10px 0px rgba(0, 0, 0, 0.04)",
         "brand-notification":
           "0px 0.7px 0.7px 0px rgba(0, 0, 0, 0.04), 0px 2.8px 7px 0px rgba(0, 0, 0, 0.04)",
+      },
+      dropShadow: {
+        "brand-text": "0px 4px 4px rgba(0, 0, 0, 0.25)",
       },
     },
   },
