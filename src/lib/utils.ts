559afddfefe1ddb8d41ec58c2ee7a18ec93e9255
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-11-11 04:11:50 +0530

    feat: added email validation utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const validEmail = (email: string): boolean => {
+  const pattern =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const specials = /[*|\":<>[\]{}`\\()';&$]/;
+
+  return pattern.test(email) && !specials.test(email);
+};
