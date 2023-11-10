90b227ace857e34a4b10792a6ebbb51a2f590c4a
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-11-11 02:43:27 +0530

    fix/refactor: re-configured tailwind css globals

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,18 +15,25 @@ export default {
           "linear-gradient(90deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.92) 27.6%, #000 53.13%, rgba(0, 0, 0, 0.67) 81.77%, rgba(0, 0, 0, 0.00) 100%)",
       },
       colors: {
-        "brand-dark-grey": "#19191A",
-        "brand-light-grey": "#96979A",
-        "brand-mild-grey": "rgba(229, 229, 230, 0.50)",
-        "brand-text": "#1D2129",
+        "brand-grey-50": "rgba(229, 229, 230, 0.50)",
+        "brand-grey-100": "#E5E5E6",
+        "brand-grey-200": "#CACBCC",
+        "brand-grey-300": "#B0B1B3",
+        "brand-grey-400": "#96979A",
+        "brand-grey-500": "#1D2129",
+        "brand-grey-800": "#313233",
+        "brand-grey-900": "#19191A",
+        "brand-neutral-100": "#F2F2F2",
+        "brand-neutral-300": "#D4D4D4",
         "brand-blue": "#0071E3",
-        "brand-border": "#D4D4D4",
       },
       boxShadow: {
         "brand-testimonial":
           "0px 1px 1px 0px rgba(0, 0, 0, 0.04), 0px 4px 10px 0px rgba(0, 0, 0, 0.04)",
         "brand-notification":
           "0px 0.7px 0.7px 0px rgba(0, 0, 0, 0.04), 0px 2.8px 7px 0px rgba(0, 0, 0, 0.04)",
+        "brand-layer":
+          "0px 14px 64px -4px rgba(24, 39, 75, 0.12), 0px 8px 22px -6px rgba(24, 39, 75, 0.12)",
       },
       dropShadow: {
         "brand-text": "0px 4px 4px rgba(0, 0, 0, 0.25)",
